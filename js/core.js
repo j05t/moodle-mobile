62d131d87894faefb9f1b7a8fc415cc9db947fb7
Nevzad Mujic <mujic.n@gmail.com>
2017-06-09 18:15:24 +0200

    updates

--- js/core.js
+++ js/core.js
@@ -83,5 +83,18 @@ var core = {
 		return function (a, b) {
 			return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
 		}
+	},
+
+	// https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+	getParameterByName: function(name, url) {
+		if (!url) {
+			url = window.location.href;
+		}
+
+		name = name.replace(/[\[\]]/g, "\\$&");
+		var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"), results = regex.exec(url);
+		if (!results) return null;
+		if (!results[2]) return '';
+		return decodeURIComponent(results[2].replace(/\+/g, " "));
 	}
 };
